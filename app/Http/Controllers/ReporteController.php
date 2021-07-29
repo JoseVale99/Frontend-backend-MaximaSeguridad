e59c1aeeed854e1574aadab64a8e7ccce77095b8
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-07-29 04:43:07 -0500

    fixed solutions!

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -19,7 +19,7 @@ class ReporteController extends Controller
         $total_mes = DB::table('pedidos')
         ->select(
             DB::raw('sum(total_venta) as total'),
-            DB::raw('TO_CHAR(pedidos.fecha,"Month") as mes'))
+            DB::raw('TO_CHAR(pedidos.fecha) as mes'))
         
         ->groupBy('mes')
         ->get();
