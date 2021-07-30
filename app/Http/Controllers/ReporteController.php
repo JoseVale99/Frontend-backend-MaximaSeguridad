fe427378b471adbc291a275746b075416cf56022
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-07-30 00:24:34 -0500

    fixed:sol

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -16,13 +16,12 @@ class ReporteController extends Controller
         $dias = ['Lunes','Martes', 'Miercoles','Jueves',
                     'Viernes','Sabado','Domingo'];
         // foreach ($year as $key => $value) {
-        $total_mes = DB::table('pedidos')
-        ->select(
-            DB::raw('sum(total_venta) as total'),
-            DB::raw('MONTHNAME(pedidos.fecha) as mes'))
-        
-        ->groupBy('mes')
-        ->get();
+        $total_mes = Pedido::select(
+            DB::raw('sum(total_venta) as sums'),
+            DB::raw("DATE_FORMAT(pedidos.fecha,'Mon-dd-YYYY') as months")
+        )
+            ->groupBy('months')
+            ->get();
 
         $total_anio = DB::table('pedidos')
         ->select(
