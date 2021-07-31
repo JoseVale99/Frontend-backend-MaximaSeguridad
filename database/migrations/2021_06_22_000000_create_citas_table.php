ad4437b5f971bb6b4c5c0f18922deb149911424c
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-07-31 16:18:37 -0500

    fixed:sol

--- database/migrations/2021_06_22_000000_create_citas_table.php
+++ database/migrations/2021_06_22_000000_create_citas_table.php
@@ -23,7 +23,7 @@ class CreateCitasTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
-            $table->string('photo')->nullable();
+            $table->string('photo')->default('default.svg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
