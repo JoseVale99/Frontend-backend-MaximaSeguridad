c1e0206d618cc4328ef43d7956bd6e71394114c6
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-08-06 17:15:58 -0500

    profile image

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -33,7 +33,7 @@
                             <div class="row">
                                 <div class="col-md-4 mt-4">
                                     <div class="text-center">
-                                        <img class="img-responsive rounded-circle" src="{{$user->photo}}" width="20%" lt="">
+                                        <img class="img-responsive rounded-circle" src="{{$user->photo}}" width="50%" alt="">
 
                                     </div>
                             </div>
